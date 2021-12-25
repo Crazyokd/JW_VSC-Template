56b60e822446a6c6cdc8a3fed21f409a434352dc
Rekord <1324596506@qq.com>
2021-12-25 17:17:31 +0800

    fix/bug:编译时动态添加依赖

--- deploy.bat
+++ deploy.bat
@@ -1,9 +1,27 @@
 dir /s /B *.java > .\sources.txt
 
+dir /s /B *.jar > .\classpath.txt
+
 @REM 设置servlet-api.jar包路径  |  请将下面的目录修改为自己电脑上的相应目录
 set CLSPT="D:\ProgramFiles2\Tomcat\apache-tomcat-8.0.50\lib\servlet-api.jar"
+
+for /f "tokens=*" %%a in (classpath.txt) do call :processline %%a
+
 set TARGET_DIR=".\WEB-INF\classes"
 
 javac -encoding UTF-8 -d %TARGET_DIR% -classpath %CLSPT% -nowarn -g:none @sources.txt
 
 del .\sources.txt
+del .\classpath.txt
+
+@REM echo %CLSPT% > out.txt
+
+goto :eof
+
+:processline
+set CLSPT=%CLSPT%;%*
+echo %CLSPT%
+
+goto :eof
+
+:eof
